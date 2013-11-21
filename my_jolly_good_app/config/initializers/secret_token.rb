503b89e6f77c9bf15f57eaeb5133ac4df041e7a8
jamiewallace <wallacejamiec@gmail.com>
2013-11-21 11:55:37 +0000

    initial commit

---
+++ my_jolly_good_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyJollyGoodApp::Application.config.secret_token = 'b5073bb146a6df371d809e34b3a15bad40e88f8ffa56c9a7655da4ebf235f9b902ef2a963ee319c61f16762efcfb7f67d096e8232f6e03362069742552b50743'
